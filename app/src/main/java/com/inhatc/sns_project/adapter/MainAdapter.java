1e45c97976c664a47405f0ce8de0d39fc843389f
admin <dlxodus16@naver.com>
2021-06-14 18:50:51 +0900

    Part14

--- app/src/main/java/com/inhatc/sns_project/adapter/MainAdapter.java
+++ app/src/main/java/com/inhatc/sns_project/adapter/MainAdapter.java
@@ -3,27 +3,33 @@ package com.inhatc.sns_project.adapter;
 
 import android.app.Activity;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.inhatc.sns_project.PostInfo;
 import com.inhatc.sns_project.R;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainAdapter extends RecyclerView.Adapter<MainAdapter.GalleryViewHolder> {
     private ArrayList<PostInfo> mDataset;
     private Activity activity;
 
-    public static class GalleryViewHolder extends RecyclerView.ViewHolder {
-        public CardView cardView;
-        public GalleryViewHolder(CardView v) {
+    static class GalleryViewHolder extends RecyclerView.ViewHolder {
+        CardView cardView;
+        GalleryViewHolder(CardView v) {
             super(v);
             cardView = v;
         }
@@ -46,15 +52,39 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.GalleryViewHol
             }
         });
 
+
         return galleryViewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull final GalleryViewHolder holder, int position) {
         CardView cardView = holder.cardView;
-        TextView textView = cardView.findViewById(R.id.textView);
-        textView.setText(mDataset.get(position).getTitle());
-        Log.e("로그: ","데이터: "+mDataset.get(position).getTitle());
+        TextView titleTextView = cardView.findViewById(R.id.titleTextView);
+        titleTextView.setText(mDataset.get(position).getTitle());
+
+        TextView createdAtTextView = cardView.findViewById(R.id.createAtTextView);
+        createdAtTextView.setText(new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(mDataset.get(position).getCreatedAt()));
+
+        LinearLayout contentsLayout = cardView.findViewById(R.id.contentsLayout);
+        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        ArrayList<String> contentsList = mDataset.get(position).getContents();
+
+        if(contentsLayout.getChildCount() == 0){
+            for(int i=0; i<contentsList.size(); i++) {
+                String contents = contentsList.get(i);
+                if (Patterns.WEB_URL.matcher(contents).matches()) {
+                    ImageView imageView = new ImageView(activity);
+                    imageView.setLayoutParams(layoutParams);
+                    contentsLayout.addView(imageView);
+                    Glide.with(activity).load(contents).override(1000).into(imageView);
+                } else {
+                    TextView textView = new TextView(activity);
+                    textView.setLayoutParams(layoutParams);
+                    textView.setText(contents);
+                    contentsLayout.addView(textView);
+                }
+            }
+        }
 
     }
 
